0c056726b5a9070ca08d26e583e1d3d4cf154715
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2022-03-12 14:37:00 +0800

    renamed order into offer, added routes

--- order/models/offerSchema.js
+++ order/models/offerSchema.js
@@ -4,26 +4,48 @@ const Schema = mongoose.Schema; // Constructor function
 // === Creation of Schema ===
 
 // This is like the template for each listing
-const orderSchema = new Schema({
+const offerSchema = new Schema({
         // Put an object here so we can have more properties like required and stuff
-        ordersummary: [{type:Object,required:true, sellerid:{type: String, required: true}}],
-        totalprice: {
-            type:Number,
+    
+
+        price: {
+            type: Number,
             required: true
         },
-        buyerid: {
+
+        itemname: {
             type: String,
             required: true
+        },
+
+        itemid:{
+            type: String,
+            required: true
+        },
+        buyerid:{
+            type: Number,
+            required: true
+        },
+
+        sellerid: {
+            type: Number,
+            required: true
+        },
+
+        offerstatus:{
+            type: String,
+            default: "Pending"
         }
-      
-      
+
+
+    
     }, {timestamps:true})
 
 // === Creation of Model ===
 // This would refer to an instance of the blog 
-const Order = mongoose.model('Order',orderSchema);
+const Offer = mongoose.model('Offer',offerSchema);
 // First argument here refers to the collection in the database collection
 // Second argument here refers to the structure. Schema is specified here to display the structure
 
 // We will call the shop as a class
-module.exports = Order;
+module.exports = Offer;
--- order/order.js
+++ order/order.js
@@ -1,80 +1,158 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Order = require("./models/orders")
+const express = require("express");
+const mongoose = require("mongoose");
+const Offer = require("./models/offerSchema");
+const bodyParser = require("body-parser");
 
+const app = express();
 
-const app = express()
+// Converts Json to javascript object
+// app.use(express.json());
+
 // Takes URL encoded data and parses it into a JavaScript object on express server
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+var jsonParser = bodyParser.json();
 
 // Connection to the mongoDB
-const dbURI = "mongodb+srv://esdt4:esdt4@nodejstutoriallearning.xssjk.mongodb.net/orderdb?retryWrites=true&w=majority"
+const dbURI =
+  "mongodb+srv://esdt4:esdt4@nodejstutoriallearning.xssjk.mongodb.net/offerdb?retryWrites=true&w=majority";
 // Use mongoose to connect to the database (ASYNC function)
-mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
-.then((res)=>{
-    console.log("Connected to the database")
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => {
+    console.log("Connected to the database");
     // Listen only when connection is established
     app.listen(port, () => {
-        console.log(`listening on port ${port}`)
+      console.log(`listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const port = 5000;
+
+// Simple implementation for now NOT using controllers
+
+// Buyer places an offer, keys in the relevant data from the UI side and sends it to the server
+// /offer/createoffer
+app.post("/offer/createoffer",  (req, res) => {
+  // From the front-end side of things
+
+  // Create a new instance at the database side
+
+  if (req.body) {
+    console.log(req.body);
+    const postreq = req.body;
+    const newOffer = new Offer({
+      price: postreq.price,
+      itemname: postreq.itemname,
+      itemid: postreq.itemid,
+      buyerid: postreq.buyerid,
+      sellerid: postreq.sellerid,
+    });
+
+    // Save the new instance to the database
+    // Async funnction
+    newOffer.save()
+      .then((result) => {
+        res.status(200)
+
+        // Edit code here in the future to get back the order id (postreq._id)
+        res.send(`Your order id is : ${result._id}`);
       })
-})
-.catch((err)=>{
-    console.log(err)
-})
-const port = 5000
+      .catch((err) => {
+        res.status(400).json({"Error": "Offer not placed"});
 
-// Mongoose sandbox
-// In the actual thing, probably have to use POST
-app.get("/add-order",(req,res)=>{
+        // console.log(err.msg)
+      });
+  } else {
+    res.status(500).send("Check your network connection");
+  }
+});
 
-    // Testing out the mongoose model
-    const order = new Order({
-        ordersummary:[
-            {"test1": 2123123012233,"sellerid": "lol1"},
-            {"test2": 12,"sellerid":"lol2"}
-        ],
-        totalprice: 123,
-        buyerid: "buyerid1",
-    })
 
-    // Save to the mongodb
-    // save is an inbuilt func of moongoose
-    order.save()
+// Buyer see offers with a status of "Pending"
+app.get("/offer/:buyerid",  (req, res) => {
+    const buyerid = req.params
+    console.log(buyerid)
+    // Async
+    Offer.find({buyerid: buyerid.buyerid, offerstatus: "Pending"})
     .then((result)=>{
-        console.log("successful")
-        res.send(result)
+        console.log(result)
+        console.log(result)
+        if (result.length > 0) {
+
+            console.log(result)
+            res.json(result)
+            // Returns a json file, which is to be interpreted at the front-end side
+
+        }
+        else{
+            res.json({"Error": "You havent placed any offers yet!"})
+        }
+
+
     })
     .catch((error)=>{
-        console.log(error)
-    })
-})
-
-// Get all the orders 
-app.get("/get-orders",(req,res)=>{
-    Order.find()
-    .then((result)=>{
-        // We can send a res.json back to the front-end side
-        res.send(result)
-    })
-    .catch((error)=>{
-        console.log(error)
+        res.status(404).send(error.msg)
     })
 })
 
 
 
 
+
+
+// // Mongoose sandbox
+// // In the actual thing, probably have to use POST
+// app.get("/add-order",(req,res)=>{
+
+//     // Testing out the mongoose model
+//     const order = new Order({
+//         ordersummary:[
+//             {"test1": 2123123012233,"sellerid": "lol1"},
+//             {"test2": 12,"sellerid":"lol2"}
+//         ],
+//         totalprice: 123,
+//         buyerid: "buyerid1",
+//     })
+
+//     // Save to the mongodb
+//     // save is an inbuilt func of moongoose
+//     order.save()
+//     .then((result)=>{
+//         console.log("successful")
+//         res.send(result)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+// })
+
+// // Get all the orders
+// app.get("/get-orders",(req,res)=>{
+//     Order.find()
+//     .then((result)=>{
+//         // We can send a res.json back to the front-end side
+//         res.send(result)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+// })
+
+// app.
 
 // ==== ROUTES =====
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//   })
 
-// Get all orders for a particular user
-// Use of GET method
+// // Get all orders for a particular user
+// // Use of GET method
 
-app.get('/order', (req, res) => {
-    res.send('Hello World!')
-  })
+// app.get('/order', (req, res) => {
+//     res.send('Hello World!')
+//   })
