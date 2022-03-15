039ded3c420954334223cffbad13ecb8a8088818
WeiTingsys <lauweiting2012@gmail.com>
2022-03-15 19:47:18 +0800

    notifications update

---
+++ marketplace/src/views/SellerView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Marketplace Homepage"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ notifications/notifications.py
@@ -0,0 +1,8 @@
+#there's 2 parts to notifications - one for  1) EMAIL SMTP for payments 2) Regular Notifs 
+
+#For the DB, need to create 3 collections ('tables') in mongoDB 
+#1) "Standard Msg"
+#2) "Notification mapping" - with the seller id, buyer id.... NEW idea: to map the notif DB to Offer instead so that you can have access to seller and buyer id 
+#3) Email SMTP standard email template
+
+
