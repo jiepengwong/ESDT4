14f2f6a633585f1b7c3f7187c3eb682d9a5b1f4b
LIM-YU-XUAN <76712654+LIM-YU-XUAN@users.noreply.github.com>
2022-03-20 16:47:38 +0800

    Seller View UI v1

---
+++ marketplace/src/views/SellerView.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="sellerView">
+    <h1>This is the Seller's View</h1>
+
+    <br><br>
+    <div>
+        <form action=" ">
+            <label for="ItemName">Name of Item: &nbsp;</label> 
+                <input type="text" id="ItemName" name="ItemName"><br><br>
+            <label for="Category">Item Category: &nbsp;</label>
+                <input type="text" id="Category" name="Category"><br><br>
+            <label for="ItemDesc">Item Description: &nbsp;</label>
+                <input type="text" id="ItemDesc" name="ItemDesc"><br><br>
+            <label for="Price">Price of Item: &nbsp;</label>
+                <input type="text" id="Price" name="Price"><br><br>
+            <label for="SellerID">Enter your ID: &nbsp;</label>
+                <input type="text" id="SellerID" name="SellerID"><br><br>
+            <label for="ItemStatus">Item Status: &nbsp;</label>
+                <input type="text" id="ItemStatus" name="ItemStatus" value="Open" readonly><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+
+    </div>
+</template>
+
+<script>
+
+//   export default {
+//     name: "SellerView",
+
+//     data() {
+//       return {
+//         items:[
+//           {
+//             name: "Item 1",
+//             price: "10",
+//             quantity: "0",
+//             sellerid: "1"
+//           },
+//           {
+//             name: "Item 2",
+//             price: "20",
+//             quantity: "0",
+//             sellerid: "1"
+
+//           },
+//           {
+//             name: "Item 3",
+//             price: "30",
+//             quantity: "0",
+//             sellerid: "2"
+
+//           }
+//         ],
+//         quantity: [1,2,3,4,5,6,7,8,9,10],
+//         selected: 0
+
+//         }
+//     },
+
+//     methods: {
+
+//       sumPrice(){
+//         let sum = 0;
+//         for(let i = 0; i < this.items.length; i++){
+//           sum += this.items[i].price * this.items[i].quantity;
+//         }
+//         return sum;
+//       },
+      
+//       summarise(){
+
+//       }
+    
+//     }
+//   }
+</script>
+
+<style scoped></style>
