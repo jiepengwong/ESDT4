1b6e246bdeedd6e9eec31c5032890b1dcaf338f9
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2022-03-24 07:34:41 +0800

    added necessary routes to the navbar

--- marketplace/src/components/Navbar.vue
+++ marketplace/src/components/Navbar.vue
@@ -1,75 +1,83 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid" >
-    <a class="navbar-brand">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand">
         <!-- <img class="img-fluid" src="@/assets/henesys.png" > -->
         Henesys MarketPlace
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-
-      <div class="col-4">
-              <input type="name" class="form-control mr-sm-1" placeholder="Search" aria-label="Search" v-model="query">
-      </div>
-
-      <div class="row mx-1">
-        
-        <div class="col-3 mx-1">
-            <button class="btn btn-outline-success" type="submit" v-on:click="search()">Search</button>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="col-4">
+          <input
+            type="name"
+            class="form-control mr-sm-1"
+            placeholder="Search"
+            aria-label="Search"
+            v-model="query"
+          />
         </div>
 
-        <div class="col-2">
-          <ul class="navbar-nav">
-        <li class="nav-item" v-for="link in links" :key="link">
-          <a class="nav-link">
-              <router-link :to="link.path">{{link.text}}</router-link>
-            </a>
-        </li>
-      </ul>
+        <div class="row mx-1">
+          <div class="col-3 mx-1">
+            <button
+              class="btn btn-outline-success"
+              type="submit"
+              v-on:click="search()"
+            >
+              Search
+            </button>
+          </div>
+
+          <div class="col-2">
+            <ul class="navbar-nav">
+              <li class="nav-item" v-for="link in links" :key="link">
+                <a class="nav-link">
+                  <router-link :to="link.path">{{ link.text }}</router-link>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      
-
-      
     </div>
-  </div>
-</nav>
-
-
+  </nav>
 </template>
 
 <script>
-
-export default{
+  export default {
     name: "Navbar",
     data() {
-        return {
-            links: [
-                { path: '/catalogue', text: 'Catalogue'},
-                { path: '/login', text: 'Login' },
-                { path: '/payment', text: 'Payment' },
-                  ],
+      return {
+        links: [
+          { path: "/catalogue", text: "Catalogue" },
+          { path: "/login", text: "Login" },
+          { path: "/offers", text: "Offers" },
+          { path: "/payment", text: "Payment" },
+          { path: "/sell", text: "Sell" },
 
-            query: ""
-        }
+        ],
+
+        query: "",
+      };
     },
     methods: {
       // Push search query into the catalogue page
-        search() {
-            this.$router.push({ path: '/', query: { q: this.query } })
-            console.log("help me")
-        }
-
+      search() {
+        this.$router.push({ path: "/", query: { q: this.query } });
+        console.log("help me");
+      },
     },
-
-
-}
-
+  };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- marketplace/src/router/index.js
+++ marketplace/src/router/index.js
@@ -48,6 +48,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Payments.vue')
   },
+  {
+    path: '/sell',
+    name: 'sell',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/SellerView.vue')
+  },
 ]
 
 const router = createRouter({
