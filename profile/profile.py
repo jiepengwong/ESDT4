efabb73c34255a6301f5c2f36e83781d40abfb41
Jay Too <76758207+toominjay@users.noreply.github.com>
2022-03-27 15:08:05 +0800

    update profile.py

--- profile/profile.py
+++ profile/profile.py
@@ -18,25 +18,25 @@ CORS(app)
 class Profile(db.Model):
     __tablename__ = "Profile_details"
 
-    userId = db.Column(db.String(64), primary_key=True)
+    user_id = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(64), nullable = False)
     email = db.Column(db.String(64), nullable=False)
     ratings = db.Column(db.Float(precision=2), nullable = True)
 
-    def __init__(self,userId,name,email,ratings):
-        self.userId = userId
+    def __init__(self,user_id,name,email,ratings):
+        self.user_id = user_id
         self.name = name
         self.email = email
         self.ratings = ratings
 
     def json(self):
-        return {"userId": self.userId, "name": self.name, "email": self.email, "ratings":self.ratings}
+        return {"user_id": self.user_id, "name": self.name, "email": self.email, "ratings":self.ratings}
         
 
 # We need to get /profile/:id, this is to get the profile details of the user 
 @app.route("/profile/<string:profile_ID>")
 def find_by_profile_ID(profile_ID):
-    profile = Profile.query.filter_by(userId=profile_ID).first()
+    profile = Profile.query.filter_by(user_id=profile_ID).first()
     if profile:
         return jsonify({
             "code":200,
@@ -50,8 +50,8 @@ def find_by_profile_ID(profile_ID):
 # We need to update profile/rating/:id, this is to update the profile ratings of the user
 @app.route("/profile/ratings/<string:Profile_Id>", methods=["PUT"])
 def update_ratings(Profile_Id):
-    if (Profile.query.filter_by(userId=Profile_Id).first()):
-        db.session.delete(Profile.query.filter_by(userId=Profile_Id).first())
+    if (Profile.query.filter_by(user_id=Profile_Id).first()):
+        db.session.delete(Profile.query.filter_by(user_id=Profile_Id).first())
         db.session.commit()
 
         data = request.get_json()
@@ -73,11 +73,11 @@ def update_ratings(Profile_Id):
 # We need to post /profile/register/, this is to register new user everytime they login with google 
 @app.route("/profile/register/<string:Profile_Id>", methods=["POST"])
 def create_account(Profile_Id):
-    if (Profile.query.filter_by(userId=Profile_Id).first()):
+    if (Profile.query.filter_by(user_id=Profile_Id).first()):
         return jsonify({
             "code":400,
             "data": {
-                "userId" : Profile_Id
+                "user_id" : Profile_Id
             },
             "message": "User has already registered"
         }), 400
@@ -93,7 +93,7 @@ def create_account(Profile_Id):
         return jsonify({
             "code":500,
             "data":{
-                "userId": Profile_Id
+                "user_id": Profile_Id
             },
             "message": "An error occured creating a new account"
         }), 500
