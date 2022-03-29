a0890180e2e4608cad95d2ffe5bf3b5526c2ea12
WeiTingsys <lauweiting2012@gmail.com>
2022-03-30 01:51:04 +0800

    notiification update, previous file got locks for exposing auth token

--- notifications/notifications.Dockerfile
+++ notifications/notifications.Dockerfile
@@ -2,5 +2,5 @@ FROM python:3-slim
 WORKDIR /usr/src/app
 COPY amqp.reqs.txt ./
 RUN python -m pip install --no-cache-dir -r amqp.reqs.txt
-COPY ./notifications.py ./amqp_setup.py ./
-CMD [ "python", "./notifications.py" ]
+COPY ./twilio_notifis.py.py ./amqp_setup.py ./
+CMD [ "python", "./twilio_notifis.py" ]
--- notifications/notifs.py
+++ notifications/notifs.py
@@ -138,11 +138,11 @@ def saveToDatabase(successMsg):
 
     one_notif = {
 
-        "Notification_ID": secrets.token_urlsafe(16),
-        "Seller_ID": "1",
-        "Buyer_ID": "1",
-        "Status": "1",
-        "Message":successMsg,
+        "Notification_ID": successMsg["Notification_ID"],
+        "Seller_ID": successMsg["Seller_ID"],
+        "Buyer_ID": successMsg["Buyer_ID"],
+        "Status":  successMsg["Status"],
+        "Message": successMsg["Message"],
         "DateTimeSQL": datetime.today()
     }
 
@@ -160,7 +160,11 @@ def saveToDatabase(successMsg):
 
     #not sure if i need this ... 
     r = requests.post(url, headers={'Content-type': 'application/json'}, json={'query': query})
+# r = requests.post(url, headers={'Content-type': 'application/json'}, json={'query': query})
 
+
+if __name__ == "__main__":  # execute this program only if it is run as a script (not by 'import')  same exchange different binding key 
+    
 
 if __name__ == "__main__":  # execute this program only if it is run as a script (not by 'import')  same exchange different binding key 
     print("\nThis is " + os.path.basename(__file__), end='')
