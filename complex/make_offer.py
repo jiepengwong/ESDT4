e2314ece54aa50436c8e4bc3c9ace497b20e469d
Jay Too <76758207+toominjay@users.noreply.github.com>
2022-04-04 17:42:20 +0800

    remove additional amqp setup

--- complex/make_offer.py
+++ complex/make_offer.py
@@ -151,7 +151,6 @@ def processMakeOffer(offer): # process the json input of /make_offer (BUYER)
 
         print('The following message will be sent to the user:' + message)
         print('\n\n-----Publishing the successful offer message with routing_key=notify.offer-----')  
-        amqp_setup.check_setup()
         amqp_setup.channel.basic_publish(
         exchange=amqp_setup.exchangename, 
         routing_key="notify.offer", 
